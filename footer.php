81a8ef82929e0641177f69070627d3bad18271fd
Claudemir786 <claudemirdosantosjunior@gmail.com>
2025-04-09 21:44:27 -0300

    update 0.4

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<body>
+
+    <div class="row m-0 text-white " style="background-color: #272343;">
+                
+        <footer class="text-center p-3" >
+            <p><Strong>Cleonardo Solutions © 2025</Strong></p>
+            <p>Developers: bigode, pelanza e Clauds</p>
+        </footer>
+    </div>
+    
+</body>
+</html>
