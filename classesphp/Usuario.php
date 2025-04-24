d05cef8d1694b4f7600af55bf3f630a5148df1ad
Eduardo Pelloso <psantanaeduardo@gmail.com>
2025-04-23 21:55:20 -0300

    2.4

--- classesphp/Usuario.php
+++ classesphp/Usuario.php
@@ -1,26 +1,18 @@
 <?php
 
     class Usuario extends Pessoa{
-        private $rgm;
+        
         private $email;
         private $senha;
 
        
 
-        public function __construct($rgm, $email, $senha){
+        public function __construct($email, $senha){
             parent::__construct($nome);
-            $this->rgm = $rgm;
             $this->email = $email;
             $this->senha = $senha;
         }
 
-        public function getRgm(){  
-            return $this->rgm;  
-        }       
-
-        public function setRgm($rgm){
-            $this->rgm = $rgm;
-        }
 
         public function getEmail(){
             return $this->email;
