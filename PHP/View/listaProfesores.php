c3fb3fd5e629f972ca8a5fa2d6b97b24c92c45f5
Claudemir786 <claudemirdosantosjunior@gmail.com>
2025-06-06 11:43:59 -0300

    5.1

--- PHP/View/listaProfesores.php
+++ PHP/View/listaProfesores.php
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Professores</title>
-</head>
-<body>
-    <?php
+
+  <?php
     include '../Controller/ProfessorController.php';    
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/css/bootstrap.min.css" rel="stylesheet" />
     <meta charset="UTF-8">
     <title>Lista de Professores</title>
 </head>
