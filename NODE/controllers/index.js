4c1161ba082f008be7cee784a27fb404e2057918
Eduardo Pelloso <psantanaeduardo@gmail.com>
2025-06-24 20:04:24 -0300

    7.9

--- NODE/controllers/index.js
+++ NODE/controllers/index.js
@@ -41,6 +41,9 @@ const{
   updateProfessor,
   deleteProfessor
 } = require("../models/DAO/UsuariosDAO");
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
 // ============================
